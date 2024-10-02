e7535e3a6d403958b53cdc303b1508c34f029b5f
samin <samintejas@gmail.com>
2024-10-02 10:25:20 +0530

    better nvim configs

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -41,7 +41,7 @@ return {
 	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query","java","go","markdown" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query","java","go","markdown" ,"html"},
 			highlight = {
 				enable = true
 			}
@@ -72,82 +72,182 @@ return {
 	end,
 },
 
--- lsp 
-
+-- LSP
 {
-	"williamboman/mason.nvim",
-	cmd = "Mason",
-	config = function()
-		require("mason").setup()
-	end,
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    config = function()
+        require("mason").setup()
+    end,
 },
 {
-	"williamboman/mason-lspconfig.nvim",
-	config = function()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "gopls" },
-		})
-	end,
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "gopls", "html", "lua_ls" },
+        })
+    end,
 },
-
 {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
+    "neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require("lspconfig")
+        
+        -- Shared on_attach function
+        local on_attach = function(client, bufnr)
+            local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+            local opts = { noremap=true, silent=true }
 
-		lspconfig.gopls.setup({
-			on_attach = function(client, bufnr)
-				local opts = { noremap=true, silent=true }
-				vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-				vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-				vim.cmd([[ autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync() ]])
-			end,
-			settings = {
-				gopls = {
-					analyses = {
-						unusedparams = true,
-					},
-					staticcheck = true,
-				},
-			},
-		})
-	end,
+            -- Enhanced keybindings
+            buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+            buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+            buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+            buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+            buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+            buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+            buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+            buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+            buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+            buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+            buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+            buf_set_keymap('n', '<leader>dgn', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+            buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+            buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+            buf_set_keymap('n', '<leader>dll', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+        end
+
+        -- gopls setup
+        lspconfig.gopls.setup({
+            on_attach = on_attach,
+            settings = {
+                gopls = {
+                    analyses = {
+                        unusedparams = true,
+                        shadow = true,
+                    },
+                    staticcheck = true,
+                    gofumpt = true,
+                },
+            },
+            init_options = {
+                usePlaceholders = true,
+            }
+        })
+
+        -- html setup
+        lspconfig.html.setup({
+            on_attach = on_attach,
+        })
+
+        -- lua_ls setup
+        lspconfig.lua_ls.setup({
+            on_attach = on_attach,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {'vim'},
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                },
+            },
+        })
+    end,
 },
 
-
--- auto completion
-
+-- Autocompletion and Snippets
 {
-	"hrsh7th/nvim-cmp",
-	config = function()
-		local cmp = require("cmp")
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
+        "rafamadriz/friendly-snippets",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					-- snipping utility can be changed
-					vim.fn["vsnip#anonymous"](args.body)
-				end,
-			},
-			mapping = {
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.close(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{ name = "path" },
-				-- Add more sources as needed
-			},
-		})
-	end,
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<CR>'] = cmp.mapping.confirm {
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                },
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+            }),
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+                { name = 'path' },
+            },
+        })
+    end,
 },
 
-{ "hrsh7th/cmp-nvim-lsp" },
-{ "hrsh7th/cmp-buffer" },
-{ "hrsh7th/cmp-path" },
-{ "hrsh7th/vim-vsnip" },
+-- Autoformatter
+{
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                go = {"gofmt", "goimports"},
+                lua = { "stylua" },
+                rust = { "rustfmt" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+        })
+
+        -- Override the default formatting function
+        vim.lsp.buf.format = function(opts)
+            conform.format(vim.tbl_extend("force", {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500,
+            }, opts or {}))
+        end
+
+        vim.keymap.set({"n","v"}, "<leader>fmt", function()
+            vim.lsp.buf.format()
+        end, {desc = "Format file or range in visual mode"})
+    end,
+}
 }
